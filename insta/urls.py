8adc412ef9e25d13d962fa6143e51e22abe38405
Shinpaza <zizimmon@naver.com>
2022-05-18 23:40:00 +0900

    .

--- insta/urls.py
+++ insta/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.conf import settings
+from insta.settings import base as settings
 from django.conf.urls.static import static
 
 urlpatterns = [
