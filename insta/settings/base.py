5074f55cd14ff4fb9d976c8a83ca20d272d7615f
Shinpaza <zizimmon@naver.com>
2022-05-18 22:39:05 +0900

    .

--- insta/settings/base.py
+++ insta/settings/base.py
@@ -134,5 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
 
-MEDIA_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
